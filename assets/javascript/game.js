b8360020a1aa10c854cc65e52fea11a2c3b76a2e
Jawed Ekhlas <jawz07r1@gmail.com>
2019-09-05 17:22:01 -0700

    second try...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
-for(i = 0; i <=20; i++){
+for(i = 0; i <=10; i++){
     console.log(i);
 }
+
+
+document.onkeyup = function(event) {
 
 var Wins = 0;
 var Losses = 0;
 var Guesses Left = 0;
 var Your Guesses So Far = 0;
+
+var computerChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var userGuess = event.key;
+
+
