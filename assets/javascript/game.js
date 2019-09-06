3cc04a9f39e2fb6b6eb489bddc6934aeb9f8577e
Jawed Ekhlas <jawz07r1@gmail.com>
2019-09-05 17:55:01 -0700

    third try....

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ for(i = 0; i <=10; i++){
 
 
 document.onkeyup = function(event) {
-
+}
 var Wins = 0;
 var Losses = 0;
 var Guesses Left = 0;
@@ -12,5 +12,5 @@ var Your Guesses So Far = 0;
 
 var computerChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var userGuess = event.key;
-
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
