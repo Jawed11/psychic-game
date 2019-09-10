ce5e91e004b4c3587377d2e5ba32353055c302f9
Jawed Ekhlas <jawz07r1@gmail.com>
2019-09-10 14:55:36 -0700

    Getting closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,48 @@
-for(i = 0; i <=10; i++){
-    console.log(i);
+let Wins = 0;
+let Losses = 0;
+let GuessesLeft = 10;
+let YourGuessesSoFar = 0;
+
+let computerChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//let userGuess = event.key;
+let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//const event = (1-10);
+console.log("BELOW IS THE COMPUTER GUESS")
+console.log(computerGuess)
+
+let WinsText = document.getElementById("Wins");
+let LossesText = document.getElementById("Losses");
+let Guesses = document.getElementById("Guesses-Left");
+
+
+for (i = 0; i <=10; i++){
+   
 }
+
 
 
 document.onkeyup = function(event) {
+    userText = event.key;
+    if (userText==computerGuess){
+        Wins++
+        alert("YOU WON")
+    }else{
+        if (GuessesLeft==0){
+        alert("YOU LOSE")
+        Losses++
+      }
+
+      
+    
+      
+    
 }
-var Wins = 0;
-var Losses = 0;
-var Guesses Left = 0;
-var Your Guesses So Far = 0;
 
-var computerChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-var userGuess = event.key;
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        
 
+};
+
+//VARIABLES AT THE VERY TOP
+//ALL YOURE FUNCTIONS
+//CALLING FUNCITONS
+    
